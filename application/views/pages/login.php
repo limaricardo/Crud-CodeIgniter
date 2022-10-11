c619d2ea3216b75eb9ba2c92691fc5ead3471e78
Ricardo <limaricardop@gmail.com>
2022-10-11 18:54:39 -0300

    Initial page style and responsiveness

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -37,8 +37,6 @@
   </head>
   <body class="text-center">
     <form class="form-signin" method="post" action="<?= base_url() ?>login/store">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
@@ -49,10 +47,9 @@
         </label>
   </div>
 	<p>
-		<a href="<?= base_url() ?>signup">Don't Have an Account?</a>
+		<a href="<?= base_url() ?>signup">Don't Have an Account? Register here!</a>
 	</p>
   <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
 </form>
 </body>
 </html>
--- application/views/pages/signup.php
+++ application/views/pages/signup.php
@@ -37,8 +37,7 @@
   </head>
   <body class="text-center">
     <form class="form-signin" method="post" action="<?= base_url() ?>signup/store">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Create an account</h1>
 
       <label for="inputName" class="sr-only">Name</label>
       <input type="text" name="name" id="inputName" class="form-control" placeholder="Your Name" required autofocus>
@@ -55,8 +54,7 @@
       <p>
         <a href="<?= base_url() ?>login">Already Have an Account?</a>
       </p>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
     </form>
 </body>
 </html>
